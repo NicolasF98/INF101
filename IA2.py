339704da101b66b07765197f1a07eea49f504684
Nicolas F <fournout.nicolas@gmail.com>
2019-09-19 10:11:17 +0200

    .

---
+++ IA2.py
@@ -0,0 +1,68 @@
+import random
+
+play = "o"
+cmpt_part = 0
+win = 0 
+es = 0
+rep = "z"
+
+
+while play != "n":
+        cmpt_part += 1
+        a = 0
+        b = 100
+        y = -1
+        i = 0
+        rep = random.randint(0,100)
+        nbr = int(input("Comment tu veux d'essais ?"))
+        while (rep != "b" and nbr != i):
+                print("Je cherche un nombre entre",a," et",b,"(",nbr-i,"essais )")
+                y = (b+a)//2
+                print(y)
+                if rep < y :
+                        print("p")
+                        b = y-1
+                elif rep > y:
+                        print("g")
+                        a = y+1
+                elif rep == y:
+                        print("b")
+                        rep = "b"
+                i=i+1
+        es = i + es
+        if rep == "b":
+                print("Gagné")
+                win+=1
+        else:
+                print("Perdu")
+        print("L'IA a gagné",win,"partis sur",cmpt_part)
+        print("L'IA a mis en moyenne", nbr/cmpt_part,"essais pour gagner")
+
+        while (rep != "b" and nbr != i):
+                print("Je cherche un nombre entre",a," et",b,"(",nbr-i,"essais )")
+                y = (b+a)//2
+                print(y)
+                if rep < y :
+                        print("p")
+                        b = y-1 
+                elif rep > y:
+                        print("g")
+                        a = y+1
+                elif rep == y:
+                        print("b")
+                        rep = "b"
+                i=i+1
+        es = i + es
+        if rep == "b":
+                print("Gagné")
+                win+=1
+        else:
+                print("Perdu")
+        print("L'IA a gagné",win,"partis sur",cmpt_part)
+        print("L'IA a mis en moyenne", nbr/cmpt_part,"essais pour gagner")
+        play = input("Voulez-vous recommencez ? (o/n)\n") 
+
+
+
+x = int(input())
+y = random.randint(0,100)
