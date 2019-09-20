9a859a4a8a48a9765526b8c5efccbc84e44eb61d
Nicolas F <fournout.nicolas@gmail.com>
2019-09-20 16:16:05 +0200

    partie bonus à faire

--- TP2_3.2.2.py
+++ TP2_3.2.2.py
@@ -1,12 +1,9 @@
 import random
 
-play = "o"
 cmpt_part = 1
 win_random = 0
 win_smart = 0 
-rep = "z"
-save_rep = rep
-val = 1
+val = 0
 
 while val != 100:
         a = 0
@@ -14,7 +11,7 @@ while val != 100:
         i = 0
         i2 = 0
         rep = "z"
-        nbr = int(input("Combien veux-tu d'essais ?"))
+        nbr = int(input("Combien veux-tu d'essais ? "))
         while (rep != "b" and nbr != i):
                 print("Je cherche un nombre entre",a," et",b,"(",nbr-i,"essais )")
                 y = random.randint(a,b)
@@ -31,16 +28,18 @@ while val != 100:
                 i += 1
         if rep == "b":
                 print("Gagné")
+                print("[",val,"]","*"*i)
                 win_random += 1
         else:
                 print("Perdu")
-        rep = save_rep
         print("L'IA aleatoire a gagné",win_random,"partis sur",cmpt_part)
         print("L'IA aleatoire a mis en moyenne", i/cmpt_part,"essais pour gagner")
 
         i = 0
         a = 0
         b = 100
+        rep = "z"
+
         while (rep != "b" and nbr != i):
                 print("Je cherche un nombre entre",a," et",b,"(",nbr-i,"essais )")
                 y = (b+a)//2
@@ -62,6 +61,7 @@ while val != 100:
                 i=i+1
         if rep == "b":
                 print("Gagné")
+                print("[",val,"]","-"*i)
                 win_smart +=1
         else:
                 print("Perdu")
