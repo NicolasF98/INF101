7218fb5337b39e95f51a4a62694e36ffcd08d5ca
Nicolas F <fournout.nicolas@gmail.com>
2019-09-20 15:54:20 +0200

    fix des problèmes liées au l'IA2

--- TP2_3.2.2.py
+++ TP2_3.2.2.py
@@ -1,67 +1,73 @@
 import random
 
 play = "o"
-cmpt_part = 0
-win = 0 
-es = 0
+cmpt_part = 1
+win_random = 0
+win_smart = 0 
 rep = "z"
+save_rep = rep
+val = 1
 
-while play != "n":
-        cmpt_part += 1
+while val != 100:
         a = 0
         b = 100
-        y = -1
         i = 0
-        rep = random.randint(0,100)
-        nbr = int(input("Comment tu veux d'essais ?"))
+        i2 = 0
+        rep = "z"
+        nbr = int(input("Combien veux-tu d'essais ?"))
         while (rep != "b" and nbr != i):
                 print("Je cherche un nombre entre",a," et",b,"(",nbr-i,"essais )")
                 y = random.randint(a,b)
                 print(y)
-                if rep < y :
+                if val < y :
                         print("p")
                         b = y-1
-                elif rep > y:
+                elif val > y:
                         print("g")
                         a = y+1
-                elif rep == y:
+                elif val == y:
                         print("b")
                         rep = "b"
-                i=i+1
-                es = i + es
+                i += 1
         if rep == "b":
                 print("Gagné")
-                win+=1
+                win_random += 1
         else:
                 print("Perdu")
-        print("L'IA aleatoire a gagné",win,"partis sur",cmpt_part)
-        print("L'IA aleatoire a mis en moyenne", nbr/cmpt_part,"essais pour gagner")
-        
+        rep = save_rep
+        print("L'IA aleatoire a gagné",win_random,"partis sur",cmpt_part)
+        print("L'IA aleatoire a mis en moyenne", i/cmpt_part,"essais pour gagner")
+
+        i = 0
+        a = 0
+        b = 100
         while (rep != "b" and nbr != i):
                 print("Je cherche un nombre entre",a," et",b,"(",nbr-i,"essais )")
-                y = (b-a)//2
-                print(y)
-                if rep < y :
-                        print("p")
-                        b = y-1
-                elif rep > y:
-                        print("g")
-                        a = y+1
-                elif rep == y:
+                y = (b+a)//2
+                if a == b:
+                        print(a)
                         print("b")
                         rep = "b"
+                else:
+                        print(y)
+                        if val < y:
+                                print("p")
+                                b = y-1
+                        elif val > y:
+                                print("g")
+                                a = y+1
+                        elif val == y:
+                                print("b")
+                                rep = "b"
                 i=i+1
-        es = i + es
         if rep == "b":
                 print("Gagné")
-                win+=1
+                win_smart +=1
         else:
                 print("Perdu")
-        print("L'IA intelligente a gagné",win,"partis sur",cmpt_part)
-        print("L'IA int a mis en moyenne", nbr/cmpt_part,"essais pour gagner")
-        play = input("Voulez-vous recommencez ? (o/n)\n") 
+        print("L'IA intelligente a gagné",win_smart,"partis sur",cmpt_part)
+        print("L'IA int a mis en moyenne", i/cmpt_part,"essais pour gagner")
+        val += 1
+        cmpt_part += 1
 
 
-
-x = int(input())
-y = random.randint(0,100)
