f39f445846cf7361eb16ddd2ac22eb69df5e1dee
Nicolas F <fournout.nicolas@gmail.com>
2019-11-18 14:59:28 +0100

    .

--- TP9/traduc3.py
+++ TP9/traduc3.py
@@ -1,2 +1,10 @@
 def init_scores():
+    i = 0
+    dico = {}
     nbr = int(input("Combien de joueurs total ? "))
+    while(i <= nbr):
+        dico[i]
+        i += 1
+    print(dico)
+
+init_scores()
